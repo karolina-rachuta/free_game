1ac5950be5b626586109b70841c4c87e1ef18e8a
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-07-22 12:33:23 +0400

    added filters and functions to change them

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import useFetch from "../hooks/useFetch";
-import GameCard from "../components/GameCard"
+import GameCard from "../components/GameCard";
+import Filters from "../filters/Filters";
 
 function GameList() {
   const [filter, setFilter] = useState({
@@ -11,11 +12,24 @@ function GameList() {
   const { games } = useFetch(filter);
 
   return (
-    <div style={{display: 'flex', flexWrap: 'wrap', width:'100%'}}>
-      {games.map(({ id, title, thumbnail, short_description, platform, genre }) => (
-        <GameCard key={id} title={title} thumbnail={thumbnail} short_description={short_description} platform={platform} genre={genre}/>
-      ))}
-    </div>
+    <>
+      <Filters setFilter={setFilter}/>
+      <div style={{ display: "flex", flexWrap: "wrap", width: "100%" }}>
+        {games.map(
+          ({ id, title, thumbnail, short_description, platform, genre }) => (
+            <GameCard
+              key={id}
+              id={id}
+              title={title}
+              thumbnail={thumbnail}
+              short_description={short_description}
+              platform={platform}
+              genre={genre}
+            />
+          )
+        )}
+      </div>
+    </>
   );
 }
 
---
+++ src/filters/constant.js
@@ -0,0 +1,127 @@
+export const PLATFORMS = [{
+        value: 'browser',
+        display: 'Browser (Web)'
+    },
+    {
+        value: 'pc',
+        display: 'Windows (PC)'
+    }
+];
+
+export const GENRES = [{
+        value: 'mmo',
+        display: 'MMO'
+    },
+    {
+        value: 'mmorpg',
+        display: 'MMORPG'
+    },
+    {
+        value: 'shooter',
+        display: 'SHOOTER'
+    },
+    {
+        value: 'strategy',
+        display: 'STRATEGY'
+    },
+    {
+        value: 'moba',
+        display: 'MOBA'
+    },
+    {
+        value: 'card',
+        display: 'Card Games'
+    },
+    {
+        value: 'racing',
+        display: 'Card Games'
+    },
+    {
+        value: 'sports',
+        display: 'Sports'
+    },
+    {
+        value: 'social',
+        display: 'Social'
+    },
+    {
+        value: 'fighting',
+        display: 'Fighting'
+    }
+]
+
+export const TAGS = [{
+        value: 'mmofps',
+        display: 'MMOFPS'
+    },
+    {
+        value: 'mmofps',
+        display: 'MMOFPS'
+    },
+    {
+        value: 'mmofps',
+        display: 'MMOFPS'
+    },
+    {
+        value: 'action-rpg',
+        display: 'ACTION-RPG'
+    },
+    {
+        value: 'sandbox',
+        display: 'Sandbox'
+    },
+    {
+        value: 'open-world',
+        display: 'Open World'
+    },
+    {
+        value: 'survival',
+        display: 'Survival'
+    },
+    {
+        value: 'battle-royale',
+        display: 'Battle Royale'
+    },
+    {
+        value: 'mmotps',
+        display: 'MMOTPS'
+    },
+    {
+        value: 'anime',
+        display: 'Anime'
+    },
+    {
+        value: 'pvp',
+        display: 'PVP'
+    },
+    {
+        value: 'pve',
+        display: 'PvE'
+    },
+    {
+        value: 'fantasy',
+        display: 'Fantasy'
+    },
+    {
+        value: 'sci-fi',
+        display: 'Sci-fi'
+    }
+]
+
+export const SORT_BY = [{
+        value: 'revelence',
+        display: 'Revelence'
+    },
+    {
+        value: 'popularity',
+        display: 'Popularity'
+    },
+    {
+        value: 'release-date',
+        display: 'Release Date'
+    },
+    {
+        value: 'alphabetical',
+        display: 'Alphabetical'
+    }
+]
